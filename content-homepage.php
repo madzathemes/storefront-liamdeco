855b9124d577dfcdcc1b88e0280c020f0709efd7
madzathemes <madza@madzathemes.com>
2018-05-02 23:06:52 +0300

    fix

--- content-homepage.php
+++ content-homepage.php
@@ -9,4 +9,6 @@
 <?php
 $featured_image = get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );
 ?>
+<div class="liamhome-slider">
 <?php echo do_shortcode('[rev_slider alias="product-showcase"]'); ?>
+</div>
